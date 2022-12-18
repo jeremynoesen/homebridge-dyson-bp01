55c3280b3e02e830e19a921bf06c7976d6ae346d
Jeremy Noesen <jeremynoesen@gmail.com>
2022-12-17 21:55:14 -0600

    clean up messages

--- src/messages.ts
+++ src/messages.ts
@@ -16,17 +16,17 @@ export const INFO_MODEL: string = "BP01";
 /**
  * Information service serial number field
  */
-export const INFO_SERIAL_NUMBER: string = "Printed under machine";
+export const INFO_SERIAL_NUMBER: string = "Printed under machine.";
 
 /**
  * Identify message when the BroadLink RM is not connected
  */
-export const IDENTIFY_NOT_CONNECTED: string = "Identified, BroadLink RM not connected";
+export const IDENTIFY_NOT_CONNECTED: string = "Identified; BroadLink RM not connected.";
 
 /**
  * Identify message when the BroadLink RM is connected
  */
-export const IDENTIFY_CONNECTED: string = "Identified, BroadLink RM connected at " + PLACEHOLDER;
+export const IDENTIFY_CONNECTED: string = "Identified; BroadLink RM connected. [" + PLACEHOLDER + "]";
 
 /**
  * Message shown when the plugin is initially searching for the BroadLink RM
@@ -36,7 +36,7 @@ export const DEVICE_SEARCHING: string = "Searching for BroadLink RM...";
 /**
  * Message shown when the BroadLink RM is discovered
  */
-export const DEVICE_DISCOVERED: string = "BroadLink RM discovered at " + PLACEHOLDER;
+export const DEVICE_DISCOVERED: string = "BroadLink RM discovered. [" + PLACEHOLDER + "]";
 
 /**
  * Message shown when the plugin is attempting to reconnect to the BroadLink RM
@@ -46,34 +46,34 @@ export const DEVICE_RECONNECTING: string = "Reconnecting to BroadLink RM...";
 /**
  * Message shown when the BroadLink RM is reconnected
  */
-export const DEVICE_RECONNECTED: string = "BroadLink RM reconnected";
+export const DEVICE_RECONNECTED: string = "BroadLink RM reconnected.";
 
 /**
  * Message shown when loading the previous active state
  */
-export const ACTIVE_INIT: string = "Active is " + PLACEHOLDER;
+export const ACTIVE_INIT: string = "Active initialized to " + PLACEHOLDER + ".";
 
 /**
  * Message shown when changing the active state
  */
-export const ACTIVE_SET: string = "Active set to " + PLACEHOLDER;
+export const ACTIVE_SET: string = "Active set to " + PLACEHOLDER + ".";
 
 /**
  * Message shown when loading the previous rotation speed
  */
-export const ROTATION_SPEED_INIT: string = "Rotation speed is " + PLACEHOLDER + "%";
+export const ROTATION_SPEED_INIT: string = "Rotation speed initialized to " + PLACEHOLDER + "%.";
 
 /**
  * Message shown when setting a new rotation speed
  */
-export const ROTATION_SPEED_SET: string = "Rotation speed set to " + PLACEHOLDER + "%";
+export const ROTATION_SPEED_SET: string = "Rotation speed set to " + PLACEHOLDER + "%.";
 
 /**
  * Message shown when changing the swing mode
  */
-export const SWING_MODE_INIT: string = "Swing mode is " + PLACEHOLDER;
+export const SWING_MODE_INIT: string = "Swing mode initialized to " + PLACEHOLDER + ".";
 
 /**
  * Message shown when changing the swing mode
  */
-export const SWING_MODE_SET: string = "Swing mode set to " + PLACEHOLDER;
+export const SWING_MODE_SET: string = "Swing mode set to " + PLACEHOLDER + ".";
